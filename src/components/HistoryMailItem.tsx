e90221002cddb48849f7d654946288875d9ae7f9
Parsa Karamat <pkmtyp@gmail.com>
2023-11-24 01:28:40 -0800

    Added DatePicker with requests to the API

--- src/components/HistoryMailItem.tsx
+++ src/components/HistoryMailItem.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-function HistoryMailItem(): JSX.Element {
+function HistoryMailItem({time}): JSX.Element {
+  let dateTimeObject = new Date(time);
+  let dateString = new Date(time).toLocaleDateString(undefined, {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+  let timeString = new Date(time).toLocaleTimeString();
+
   return (
     <View
       style={{
         height: 80,
         width: 350,
-        alignItems: "center",
+        alignItems: 'center',
         backgroundColor: '#E1F8DB',
         borderRadius: 20,
         borderWidth: 1,
@@ -16,9 +24,11 @@ function HistoryMailItem(): JSX.Element {
         justifyContent: 'space-between',
       }}>
       <Text style={{color: 'black', fontSize: 22, fontWeight: 'bold'}}>
-        9:00 PM
+        {timeString}
       </Text>
-      <Text style={{color: 'black', fontSize: 22, fontWeight: 'bold'}}>Sept 1, 2023</Text>
+      <Text style={{color: 'black', fontSize: 22, fontWeight: 'bold'}}>
+        {dateString}
+      </Text>
     </View>
   );
 }
--- src/views/History.tsx
+++ src/views/History.tsx
@@ -1,10 +1,61 @@
-import React, {useState} from 'react';
-import {View, TextInput, FlatList} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  TextInput,
+  FlatList,
+  Button,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from 'react-native';
 import HistoryMailItem from '../components/HistoryMailItem';
+import DatePicker from 'react-native-date-picker';
+import {REACT_APP_API_BASE_URL} from '@env';
+
+function dateToStringLongFormat(date: Date): string {
+  return date.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+}
+
+function dateToStringISOFormat(date: Date): string {
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const year = date.getFullYear();
+  return `${month}-${day}-${year}`;
+}
+
+function getDateFromToday(days, months, years) {
+  let date = new Date();
+  date.setDate(date.getDate() + days);
+  date.setMonth(date.getMonth() + months);
+  date.setFullYear(date.getFullYear() + years);
+  return date;
+}
 
 function History(): JSX.Element {
-  const [startDate, onStartDateChange] = useState('Sept 1, 2023');
-  const [endDate, onEndDateChange] = useState('Sept 30, 2023');
+  const [startDate, setStartDate] = useState(getDateFromToday(0, 0, -1));
+  const [endDate, setEndDate] = useState(new Date());
+  const [startDateOpen, setStartDateOpen] = useState(false);
+  const [endDateOpen, setEndDateOpen] = useState(false);
+  const [mailData, setMailData] = useState([]);
+
+  const getMailData = () => {
+    let startDateStr = dateToStringISOFormat(startDate);
+    let endDateStr = dateToStringISOFormat(endDate);
+    fetch(`${REACT_APP_API_BASE_URL}/mail/${startDateStr}/${endDateStr}`)
+      .then(res => res.json())
+      .then(data => setMailData(data))
+      .catch(err => console.log('err', err));
+  };
+
+  useEffect(() => {
+    getMailData();
+  }, [startDate, endDate]);
+
+  const renderMailCards = ({item}) => <HistoryMailItem time={item.time} />;
 
   return (
     <View style={{gap: 10}}>
@@ -17,30 +68,72 @@ function History(): JSX.Element {
           padding: 10,
           backgroundColor: '#9BD1A4',
         }}>
-        <TextInput
-          style={{backgroundColor: 'white'}}
-          value={startDate}
-          onChangeText={text => onStartDateChange(text)}
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => setStartDateOpen(true)}>
+          <Text style={styles.buttonText}>
+            {dateToStringLongFormat(startDate)}
+          </Text>
+        </TouchableOpacity>
+        <DatePicker
+          modal
+          open={startDateOpen}
+          mode="date"
+          date={startDate}
+          maximumDate={endDate}
+          onConfirm={date => {
+            setStartDateOpen(false);
+            setStartDate(date);
+          }}
+          onCancel={() => {
+            setStartDateOpen(false);
+          }}
         />
-        <TextInput
-          style={{backgroundColor: 'white'}}
-          value={endDate}
-          onChangeText={text => onEndDateChange(text)}
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => setEndDateOpen(true)}>
+          <Text style={styles.buttonText}>
+            {dateToStringLongFormat(endDate)}
+          </Text>
+        </TouchableOpacity>
+        <DatePicker
+          modal
+          open={endDateOpen}
+          mode="date"
+          date={endDate}
+          minimumDate={startDate}
+          onConfirm={date => {
+            setEndDateOpen(false);
+            setEndDate(date);
+          }}
+          onCancel={() => {
+            setEndDateOpen(false);
+          }}
         />
       </View>
-      <View style={{alignItems: 'center', gap: 15}}>
-        <HistoryMailItem />
-        <HistoryMailItem />
-        <HistoryMailItem />
-        <HistoryMailItem />
-        <HistoryMailItem />
-        <HistoryMailItem />
-        <HistoryMailItem />
-        <HistoryMailItem />
-        <HistoryMailItem />
-      </View>
+      <FlatList
+        data={mailData}
+        renderItem={renderMailCards}
+        keyExtractor={item => item.mid}
+        contentContainerStyle={{alignItems: 'center'}}
+        ListFooterComponent={<View style={{marginBottom: 100}} />}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 156,
+    height: 35,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+});
 
 export default History;
